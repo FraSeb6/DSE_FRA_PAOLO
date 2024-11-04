c3a7b70fad99c55af19f90e4b94e0cd25d780cca
Francesco <francescosebastiano.memmola@studenti.unimi.it>
2024-11-04 14:45:26 +0100

    Graphic reppresentation

--- Weather/main.py
+++ Weather/main.py
@@ -3,7 +3,6 @@ import os.path
 import zipfile
 import pandas as pd
 import urllib.request
-import matplotlib.pyplot as plt
 
 
 
@@ -27,22 +26,36 @@ global_temperature_city_csv          = pd.read_csv('.\\dataset\\GlobalLandTemper
 
 country = input("Enter the country name: ")
 
-# Filter the data for the specified country
+import matplotlib.pyplot as plt
+
+# Filter the dataframe for the given country
 country_data = global_temperature_country_df[global_temperature_country_df['Country'] == country]
+#for key, value in global_temperature_country_csv.items():
+# Check if the country exists in the dataset
+while True:
+    if country_data.empty:
+        print(f"No data available for {country}. Please try again.")
+        country = input("Enter the country name: ")
+        country_data = global_temperature_country_df[global_temperature_country_df['Country'] == country]
+    else:
+        break
 
-if country_data.empty:
-    print(f"No data available for {country}")
-else:
-    # Convert the 'dt' column to datetime
-    country_data['dt         '] = pd.to_datetime(country_data['dt         '])
+# Convert the 'dt' column to datetime
+country_data['dt'] = pd.to_datetime(country_data['dt'])
 
-    # Plot the data
-    plt.figure(figsize=(10, 5))
-    plt.plot(country_data['dt         '], country_data['AverageTemperature'], label=f'Temperature in {country}')
-    plt.xlabel('Year')
-    plt.ylabel('Average Temperature (°C)')
-    plt.title(f'Average Temperature in {country} Over Time')
-    plt.legend()
-    plt.grid(True)
-    plt.show()
-    
+# Ask the user for the start and end year
+start_year = int(input("Enter the start year min 1750: "))
+end_year = int(input("Enter the end year max 2013: "))
+
+# Filter the data for the selected years
+country_data = country_data[(country_data['dt'].dt.year >= start_year) & (country_data['dt'].dt.year <= end_year)]
+
+# Plot the data
+plt.figure(figsize=(10, 5))
+plt.plot(country_data['dt'], country_data['AverageTemperature'], label='Average Temperature')
+plt.xlabel('Year')
+plt.ylabel('Average Temperature (°C)')
+plt.title(f'Average Temperature Over Time in {country} from {start_year} to {end_year}')
+plt.legend()
+plt.grid(True)
+plt.show()
