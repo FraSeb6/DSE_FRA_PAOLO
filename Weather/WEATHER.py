8d44b98cfc706d4609d65e38123ef81c884c622b
Francesco <francescosebastiano.memmola@studenti.unimi.it>
2024-11-07 21:00:03 +0100

    m

--- Weather/WEATHER.py
+++ Weather/WEATHER.py
@@ -1,4 +1,14 @@
+import os
 import streamlit as st
+
+import os
+
+# Ottieni la directory dello script corrente
+script_directory = os.path.dirname(os.path.abspath(__file__))
+
+# Imposta la directory di lavoro sulla posizione dello script
+os.chdir(script_directory)
+
 
 # Titolo dell'app
 st.title("Benvenuto nella mia prima app con Streamlit!")
